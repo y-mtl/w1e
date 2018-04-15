b9d4b7e591ced16904cbf70114bb0f4a652f25c1
Yasu <yassantune@gmail.com>
2018-04-15 21:12:06 +0000

    in the callback, added function(response). response is required as an argument.

--- https_example.js
+++ https_example.js
@@ -8,10 +8,13 @@ var options = {
 //
 
 // called by https when the request is made.
-var callback = function() {
+// var callback = function() {
+//   console.log('In response handler callback!');
+// }
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
-
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
