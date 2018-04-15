4c0dfa889c64a8c8638ae95f5c86328a20cda8c7
Yasu <yassantune@gmail.com>
2018-04-15 21:03:20 +0000

    callback is returned at the end.

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+//console.log('I did it!');
+//
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+// callback happens after the response is returned
